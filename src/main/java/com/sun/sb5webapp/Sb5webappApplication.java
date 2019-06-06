e8c836f20cae299b4df67bf15588157f6734a379
Huynh Nguyen V <nguyenviethuynh1996.vih@gmail.com>
2019-06-07 00:14:24 +0700

    create project

---
+++ src/main/java/com/sun/sb5webapp/Sb5webappApplication.java
@@ -0,0 +1,13 @@
+package com.sun.sb5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb5webappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb5webappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sun/sb5webapp/Sb5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sun.sb5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb5webappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
